01f3b4300d8990a9363dbae18e8c7cd6635ddc9c
Florian CHICOT <florian.chicot@outlook.com>
2024-04-05 15:18:59 +0200

    Update launcher and view to display buttons and ajout formulaire labels

--- src/gui/view/TacheView.java
+++ src/gui/view/TacheView.java
@@ -1,7 +1,7 @@
 package gui.view;
 
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class TacheView extends JPanel {
 
@@ -9,17 +9,30 @@ public class TacheView extends JPanel {
     private JButton ajouterButton;
     private JButton modifierButton;
     private JButton supprimerButton;
+    private JPanel formulaireAjoutPanel;
+    private JPanel activePanel;
+
+    private JLabel titreLabel;
+    private JLabel descriptionLabel;
+    private JLabel dateLabel;
 
     public TacheView() {
+        this.activePanel = new JPanel();
         this.mainPanel = new JPanel();
-        this.ajouterButton = new JButton("Ajouter une tâche");
-        this.modifierButton = new JButton("Modifier une tâche");
-        this.supprimerButton = new JButton("Supprimer une tâche");
+        this.ajouterButton = new JButton("Ajouter");
+        this.modifierButton = new JButton("Modifier");
+        this.supprimerButton = new JButton("Supprimer");
+
+        this.formulaireAjoutPanel = new JPanel();
+        this.titreLabel = new JLabel("Titre : ");
+        this.descriptionLabel = new JLabel("Description : ");
+        this.dateLabel = new JLabel("Date : ");
 
         initGUI();
     }
 
     private void initGUI() {
+        this.activePanel = this.mainPanel;
         addButton(mainPanel, ajouterButton);
         addButton(mainPanel, modifierButton);
         addButton(mainPanel, supprimerButton);
@@ -29,7 +42,30 @@ public class TacheView extends JPanel {
         panel.add(button);
     }
 
-    public Component getMainPanel() {
+    private void addLabel(JPanel panel, JLabel label) {
+        panel.add(label);
+    }
+
+    public JPanel getActivePanel() {
+        return this.activePanel;
+    }
+
+    public JPanel getMainPanel() {
         return this.mainPanel;
+    }
+
+    public JPanel getFormulaireAjoutPanel() {
+        return this.formulaireAjoutPanel;
+    }
+
+    public void generateAjouterButtonListener(ActionListener listener) {
+        this.ajouterButton.addActionListener(listener);
+    }
+
+    public void creerFormulaireAjout() {
+        this.activePanel = this.formulaireAjoutPanel;
+        addLabel(formulaireAjoutPanel, titreLabel);
+        addLabel(formulaireAjoutPanel, descriptionLabel);
+        addLabel(formulaireAjoutPanel, dateLabel);
     }
 }
